4969170ab52baaeb145c4226fbc3809e254b58cd
Sergio NUNEZ <sergio.nunez@laposte.net>
2025-06-20 14:50:12 +0200

    Vue basic template added
    - Tailwin DSN script
    - Partials : footer and header

--- vue/templates/errors/default.php
+++ vue/templates/errors/default.php
@@ -1,1 +1,9 @@
-<h1><?=$errorMessage?></h1>
+<?php require_once APP_ROOT . ("/vue/templates/_partials/_header.php");?>
+<main>
+    <section class="text-gray-400 body-font bg-gray-900">
+        <div class="container flex flex-wrap px-5 py-24 mx-auto items-center">
+            <h1 class="text-red-600 font-bold"><?=$errorMessage?></h1>
+        </div>
+    </section>
+</main>
+<?php require_once APP_ROOT . ("/vue/templates/_partials/_footer.php");?>
